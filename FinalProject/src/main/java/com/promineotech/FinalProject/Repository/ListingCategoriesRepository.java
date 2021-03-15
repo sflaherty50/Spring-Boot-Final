24e04d03d491950bb3b5dcf521cbcfbf33aa7b86
sflaherty50 <72477335+sflaherty50@users.noreply.github.com>
2021-03-15 18:44:14 -0400

    Delete FinalProject directory

--- FinalProject/src/main/java/com/promineotech/FinalProject/Repository/ListingCategoriesRepository.java
+++
@@ -1,5 +0,0 @@
-package com.promineotech.FinalProject.Repository;
-import org.springframework.data.repository.CrudRepository;
-import com.promineotech.FinalProject.Entity.ListingCategories;
-public interface ListingCategoriesRepository extends CrudRepository<ListingCategories, Long>{
-}
